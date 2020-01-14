d4e04c222344ae1abfffdcf2151622e88c4d27ed
Soya Uehara <e195752@ie.u-ryukyu.ac.jp>
2020-01-14 09:07:14 +0900

    1st

---
+++ src/main/java/jp/ac/uryukyu/ie/e195752/Read.java
@@ -0,0 +1,10 @@
+package jp.ac.uryukyu.ie.e195752;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Read {
+
+}
