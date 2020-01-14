9ef2afb31535e00bae334f4b6fb487cc723b54c7
Soya Uehara <e195752@ie.u-ryukyu.ac.jp>
2020-01-14 17:53:02 +0900

    2nd

--- src/main/java/jp/ac/uryukyu/ie/e195752/Main.java
+++ src/main/java/jp/ac/uryukyu/ie/e195752/Main.java
@@ -1,6 +1,13 @@
 package jp.ac.uryukyu.ie.e195752;
-
 public class Main {
     public static void main(String[] args){
+        Read DW = new Read("Damage_wall");
+        Read ブロック = new Read("Block");
+        Read 減速壁 = new Read("Deceleration_Wall");
+        Read GB = new Read("Gravity_Barrier");
+        Read 魔法陣 = new Read("Magic_Circle");
+        Read 地雷 = new Read("Mines");
+        Read ワープ = new Read("Warp");
+        Read ウィンド = new Read("wind");
     }
 }
--- src/main/java/jp/ac/uryukyu/ie/e195752/Read.java
+++ src/main/java/jp/ac/uryukyu/ie/e195752/Read.java
@@ -6,5 +6,22 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Read {
+    ArrayList<String> List = new ArrayList<String>();
+    public Read(String gimmick){
+        try {
+            File file = new File("/Users/e195752/IdeaProjects/report6/"+gimmick+".txt");
+            Scanner scan = new Scanner(file);
+            while (scan.hasNext()){
+                String str = scan.next();
+                this.List.add(str);
+            }
+            System.out.println(List.size());
+
+        }catch(FileNotFoundException e){
+            System.out.println("ファイルがありません");
+        }
+
+    }
+
 
 }
